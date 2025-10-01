a1c663d1d900158039fc3769bfe87b82186a78fd
aires1294 <pedrotja.engenharia@gmail.com>
2025-10-01 17:06:10 -0300

    Ajusta rotas

--- horus_projetct/src/app/app-routing.module.ts
+++ horus_projetct/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartilhasComponent } from './cartilhas.component';
+import { RecursosAjudaComponent } from './recursos-ajuda.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CartilhasComponent},
+  { path: 'materiais', component: CartilhasComponent },
+  { path: 'ajuda', component: RecursosAjudaComponent },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
