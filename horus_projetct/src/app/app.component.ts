afc455a4863937ca6fc3cd66dbfc8a569b42b371
aires1294 <pedrotja.engenharia@gmail.com>
2025-10-01 17:18:05 -0300

    app.component.ts

--- horus_projetct/src/app/app.component.ts
+++ horus_projetct/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ContentService, Material } from './content.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Prevenção Dependente Química';
+
+  previewMaterials: Material[] = [];
+  previewHelp: any[] = [];
+
+  constructor(private contentService: ContentService) {}
+
+  ngOnInit() {
+    this.contentService.getMaterials().subscribe(materials => {
+      this.previewMaterials = materials.slice(0, 2); // mostra só 2
+    });
+
+    this.contentService.getHelpResources().subscribe(resources => {
+      this.previewHelp = resources.slice(0, 2); // mostra só 2
+    });
+  }
 }
